f6331c752fa7385988f6cb289cf714d36e88cb3b
Gustavo Toro <tavoludra@gmail.com>
2023-08-19 15:20:27 -0500

    Initial Commit

---
+++ Fozyl/FozylApp.swift
@@ -0,0 +1,17 @@
+//
+//  FozylApp.swift
+//  Fozyl
+//
+//  Created by GAPT on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FozylApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
