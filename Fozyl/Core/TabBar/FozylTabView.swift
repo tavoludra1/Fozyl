05e3087b467031d48b5649e2c25390d98e1d2096
Gustavo Toro <tavoludra@gmail.com>
2023-08-20 17:17:09 -0500

    UI - TabBar creación de la vista que cambia en múltiples vistas (Hijas) - home, search, profile

---
+++ Fozyl/Core/TabBar/FozylTabView.swift
@@ -0,0 +1,54 @@
+//
+//  FozylTabView.swift
+//  Fozyl
+//
+//  Created by GAPT on 20/08/23.
+//
+
+import SwiftUI
+
+struct FozylTabView: View {
+    
+    // Estados
+    @State private var selectedTab = 0
+    
+    var body: some View {
+        
+        // ayuda a hacer un seguimiento de que pestania esta activa
+        TabView(selection: $selectedTab) {
+            
+            // items
+            Text("Home")
+                .tabItem {
+                    Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                        .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+                .onAppear { selectedTab = 1}
+                .tag(1)
+            
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: selectedTab == 2 ? "person.fill" : "person")
+                        .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                }
+                .onAppear{ selectedTab = 2 }
+                .tag(2)
+            
+        }
+        .tint(.black)
+    }
+}
+
+struct FozylTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        FozylTabView()
+    }
+}
